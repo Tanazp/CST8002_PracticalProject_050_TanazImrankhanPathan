ca9084e2ec4b59f826d251739fd400ba186279b0
Tanazp <pathantanazkhan03@gmail.com>
2025-03-16 15:58:56 -0400

    Modified Facility class to inherit from Record and override display method.

--- model/facility.py
+++ model/facility.py
@@ -1,12 +1,14 @@
-class Facility:
+# models/facility.py
+from models.record import Record
+
+class Facility(Record):
     def __init__(self, region, district, license_number, facility_name, facility_type,
                  facility_address_1, facility_address_2, facility_address_3, 
                  max_children, max_infants, max_preschool, max_school_age, 
                  language_of_service, operator_id, designated_facility):
+        super().__init__(license_number, facility_name)
         self.region = region
         self.district = district
-        self.license_number = license_number
-        self.facility_name = facility_name
         self.facility_type = facility_type
         self.facility_address_1 = facility_address_1
         self.facility_address_2 = facility_address_2
@@ -19,5 +21,6 @@ class Facility:
         self.operator_id = operator_id
         self.designated_facility = designated_facility
 
-    def __str__(self):
-        return f"Facility {self.facility_name} ({self.license_number}) - {self.region}, {self.district}"
+    def display(self):
+        return f"Facility Name: {self.name}, License Number: {self.license_number}, Region: {self.region}"
+
